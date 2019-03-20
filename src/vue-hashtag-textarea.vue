47266b4d3c1cda29bf2b18f2d5969d2184e27b69
Mitsuya.WATANABE <watanabe@gocco.co.jp>
2019-03-20 18:33:44 +0900

    Update library

--- src/vue-hashtag-textarea.vue
+++ src/vue-hashtag-textarea.vue
@@ -1,13 +1,104 @@
+<template>
+  <div id="input" class="hashtag-textarea">
+    <div 
+      id="input-overlay"
+      class="hashtag-textarea__overlay"
+      :style=overlayStyle />
+    <div 
+      id="input-true-text"
+      class="hashtag-textarea__true-text"
+      wrap="soft"
+      :style=trueTextStyle
+      contentEditable="true"
+      spellcheck="false">
+    </div>
+
+    <div class="hashtag-textarea__placeholder" v-show="shouldShowPlaceholder">
+      {{ option.placeholder }}
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+  // TODO: Get proper font. Should load this from local.
+  @import url(http://fonts.googleapis.com/earlyaccess/notosansjapanese.css);
+  $myFont: 14px 'Noto Sans Japanese', sans-serif;
+  $textWidth: 100%;
+
+  // NOTE: Component of hashtag textarea
+  .hashtag-textarea {
+    position: relative;
+    overflow: auto;
+    border: 1px solid #ccc;
+    background: #f9f9f9;
+    min-height: 150px;
+    height: 100%;
+
+    // NOTE: Textarea layout. Fix position
+    &__true-text {
+      z-index: 2;
+      border: 0;
+      background: transparent;
+      position: absolute;
+      top: 0;
+      color: transparent;
+      caret-color: black;
+      outline: none;
+    }    
+
+    &__overlay {
+      color: black;
+      
+      // NOTE: Hashtag layout
+      .hashtag {
+        background: #fff000; 
+        font: $myFont;
+      }        
+    }  
+
+    &__overlay, &__true-text {
+      padding: 10px;
+      margin: 0px;
+      font: $myFont;
+      height: 100%;
+      width: $textWidth;
+      white-space: pre-wrap;
+      overflow-wrap: break-word;
+    }
+
+    &__placeholder {
+      padding: 10px;
+      position: absolute;
+      top: 0;
+      color: lightgray;
+    }
+  }
+</style>
+
 <script>
 export default {
   name: 'VueHashtagTextarea',
+  props: {
+    option: {
+      type: Object,
+      default: _ => (
+        {
+          textColor: 'black',
+          font: '14px "Noto Sans Japanese", sans-serif',
+          hashtagBackgroundColor: 'transparent',
+          hashtagColor: '#ff0000',
+          placeholder: 'Sentence for placeholder #place #holder'
+        }
+      )
+    }
+  },
   data() {
     return {
       hashtagList: [],
       shouldShowPlaceholder: true
     }
   },
-  mounted() {
+  mounted() {    
     // NOTE: Create Observer. This is called when text content of the target is changed
     const target = document.getElementById('input-true-text');
     const observer = new MutationObserver(this.onObserveElement);    
@@ -19,6 +110,43 @@ export default {
                   };
     
     observer.observe(target, config);
+  },
+  computed: {
+    isSafariBrowser: function() {
+      let isSafariBrowser = false
+      const userAgent = window.navigator.userAgent.toLowerCase();
+      
+      if(userAgent.indexOf('safari') !== -1 &&
+        userAgent.indexOf('chrome') === -1 ) {
+        isSafariBrowser = true
+      }
+      
+      return isSafariBrowser;
+    },
+    trueTextStyle: function() {
+      let style = {}
+      style.font = this.option.font
+      
+      return style
+    },
+    overlayStyle: function() {
+      let style = {}
+      style.color = this.option.textColor
+      style.font = this.option.font
+
+      return style
+    },
+    hashtagStyle: function() {
+      let style = 'style="'
+      style += 'color:' + this.option.hashtagColor + ';'
+      style += 'background:' + this.option.hashtagBackgroundColor + ';'
+      style += '"'
+
+      return style
+    },
+    regExp: function() {
+      return /#[Ａ-Ｚａ-ｚA-Za-z一-龥0-9０-９ぁ-ヶｦ-ﾟー]+/g;
+    }
   },
   methods: {
     onObserveElement(mutations) {
@@ -62,9 +190,10 @@ export default {
       // NOTE: Create new text for hashtag highlight to insert element
       //       If debugging on chrome, use content2 above, otherwise
       //       line break point will be failed.
-      const exp = /#[Ａ-Ｚａ-ｚA-Za-z一-龥0-9０-９ぁ-ヶｦ-ﾟー]+/g;
-      const replaceContent = content.replace(exp, function(match) {
-        const result = '<i class="hashtag">' + match + '</i>'        
+      const srcContent = this.isSafariBrowser ? content : content2
+      const self = this
+      const replaceContent = srcContent.replace(this.regExp, function(match) {
+        const result = '<i ' + self.hashtagStyle + '>' + match + '</i>'        
         return result
       })
       
@@ -75,96 +204,33 @@ export default {
       this.hashtagList = this.getHashtagList(content)      
     },
     getHashtagList(value) {
-      const exp = /#[Ａ-Ｚａ-ｚA-Za-z一-龥0-9０-９ぁ-ヶｦ-ﾟー]+/g;
-      // const result = exp.test(value);
-      const result = value.match(exp)
+      const result = value.match(this.regExp)
       return result;
+    },
+    diffArray(newVal, oldVal) {
+      return newVal.concat(oldVal)
+        .filter(item => !oldVal.includes(item));
     }    
   },
   watch: {
     hashtagList: function(newVal, oldVal) {
-      if (newVal !== oldVal) {
-        this.$emit('onChangedHashtagList', newVal)        
+
+      const isEqualVal = JSON.stringify(newVal) === JSON.stringify(oldVal)
+      if (!isEqualVal) {
+        const hashtagData = {}
+        if (newVal === null) {
+          hashtagData.target = ''
+        } else if (oldVal === null) {
+          hashtagData.target = newVal[0]
+        } else {
+          const diff = this.diffArray(newVal, oldVal)
+          hashtagData.target = diff.length === 0 ? newVal[newVal.length - 1] : diff[0]
+        }
+
+        hashtagData.hashtags = newVal
+        this.$emit('onChangedHashtag', hashtagData)        
       }    
     }
   }
 }
 </script>
-
-<template>
-  <div id="input" class="hashtag-textarea">
-    <div 
-      id="input-overlay"
-      class="hashtag-textarea__overlay" />
-    <div 
-      id="input-true-text"
-      class="hashtag-textarea__true-text"
-      placeholder="説明を入力する#テキスト#テキスト"
-      wrap="soft"
-      contentEditable="true"
-      spellcheck="false">
-    </div>
-
-    <div class="hashtag-textarea__placeholder" v-show="shouldShowPlaceholder">
-      This is placeholder #place #holder
-    </div>
-  </div>
-</template>
-
-<style lang="scss">
-  // TODO: Get proper font. Should load this from local.
-  @import url(http://fonts.googleapis.com/earlyaccess/notosansjapanese.css);
-  $myFont: 14px 'Noto Sans Japanese', sans-serif;
-  $textWidth: 100%;
-
-  // NOTE: Component of hashtag textarea
-  .hashtag-textarea {
-    position: relative;
-    overflow: auto;
-    border: 1px solid #ccc;
-    background: #f9f9f9;
-    min-height: 150px;
-    height: 100%;
-
-    // NOTE: Textarea layout. Fix position
-    &__true-text {
-      z-index: 2;
-      border: 0;
-      background: transparent;
-      position: absolute;
-      top: 0;
-      // color: transparent;
-      caret-color: red;
-      outline: none;
-    }    
-
-    &__overlay {
-      // color: black;
-      color:red;
-      
-      // NOTE: Hashtag layout
-      .hashtag {
-        background: #fff000; 
-        color: red;
-        font: $myFont;
-      }        
-    }  
-
-    &__overlay, &__true-text {
-      padding: 10px;
-      margin: 0px;
-      font: $myFont;
-      height: 100%;
-      width: $textWidth;
-      white-space: pre-wrap;
-      overflow-wrap: break-word;
-    }
-
-    &__placeholder {
-      padding: 10px;
-      position: absolute;
-      top: 0;
-      color: lightgray;
-    }
-  }
-</style>
